fc4f303d219f920938c19b2fd2191664a752d311
Mutasem <mutasem@n8n.io>
2026-04-25 17:49:53 +0200

    internal token

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 htpasswd -bc /etc/nginx/.htpasswd "${BASIC_AUTH_USER}" "${BASIC_AUTH_PASS}"
 
 # Substitute BACKEND_URL into nginx config template
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL} ${INTERNAL_TOKEN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
